03d3ec606389544e6de74d2f86fce374ff71a771
Anurag Mishra <doitmywayweb@gmail.com>
2023-09-08 19:15:54 +0530

    Installation of pre-requisites packages

---
+++ Pre-requisites_installation.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import keras as kr
+import cv2 as cv
+
+"""
+All three packages as pre-requisites have
+been installed in the local computer"""
+
+a = tf()
+b = kr()
+c = cv()
